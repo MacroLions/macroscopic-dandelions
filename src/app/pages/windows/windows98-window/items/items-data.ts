a2c80c7bfde6895810e75dc625b5fe40fa7eb2a7
MacroLions <maitreyiperez@gmail.com>
2025-04-13 00:37:31 -0600

    Fix: Amarillo dandelion (?)

--- src/app/pages/windows/windows98-window/items/items-data.ts
+++ src/app/pages/windows/windows98-window/items/items-data.ts
@@ -9,7 +9,7 @@ export const ITEMS_DATA: ItemData[] = [
   { name: 'Dandelion', icon: 'material-symbols-light:book-4', style:"text-purple-500"},
   { name: 'Scopisch', icon: 'lucide:file-pen-line', style: "text-fuchsia-200" },
   { name: 'Regan', icon: 'material-symbols:file-copy-off', style: "text-violet-950/90" },
-  { name: 'Macro', icon: 'mdi:microphone', style: "text-blue-500" },
+  { name: 'Macro', icon: 'mdi:microphone', style: "text-yellow-500" },
   { name: 'Dear Writer', icon: 'mdi:pen', style: "text-fuchsia-200" },
   { name: 'Dear Reader', icon: 'mdi:book-open', style: "text-yellow-950" },
   { name: 'Lion', icon: 'mdi:paw', style: "text-green-500" },
